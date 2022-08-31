c3921b6947bcb63e66b641f0254f2340db701756
Jefferson Santos da Silva <jeffersonadm.rn@hotmail.com>
2022-08-31 17:07:28 -0300

    adicionado os requisitos 2 e 3 do projeto

---
+++ JavaScript.js
@@ -0,0 +1,24 @@
+
+function alterarCorBlack(){
+  let corBlack = document.getElementsByClassName('color')[0];
+  corBlack.style.backgroundColor = "black";
+}
+alterarCorBlack()
+
+function alterarCorGreen(){
+  let corGreen = document.getElementsByClassName('color')[1];
+  corGreen.style.backgroundColor = "green";
+}
+alterarCorGreen()
+
+function alterarCorRed(){
+  let corRed = document.getElementsByClassName('color')[2];
+  corRed.style.backgroundColor = "red";
+}
+alterarCorRed()
+
+function alterarCorBlue(){
+  let corBlue = document.getElementsByClassName('color')[3];
+  corBlue.style.backgroundColor = "blue";
+}
+alterarCorBlue()
